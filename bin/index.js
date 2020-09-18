b3d6d5a365bcd91fcba693ac6303ee352952c506
saba-coding <saba.chikhina@gmail.com>
2020-09-18 12:18:30 +0400

    Updates

--- bin/index.js
+++ bin/index.js
@@ -53,14 +53,18 @@ for (const triggerName in config.triggers) {
 }
 
 const dest = path.join(__dirname, '../../../', config.destFolder)
-fs.mkdirSync(path.join(dest, 'functions'))
-fs.mkdirSync(path.join(dest, 'triggers'))
+
+const isFunctionsDefined = fs.existsSync(path.join(dest, 'functions'))
+const isTriggersDefined = fs.existsSync(path.join(dest, 'triggers'))
+
+if (!isFunctionsDefined) { fs.mkdirSync(path.join(dest, 'functions')) }
+if (!isTriggersDefined) { fs.mkdirSync(path.join(dest, 'triggers')) }
 
 for (const file of files) {
   switch (file.type) {
     case 'function':
       const folderPath = path.join(dest, 'functions', file.name)
-      fs.mkdirSync(folderPath)
+      if (!fs.existsSync(folderPath)) { fs.mkdirSync(folderPath) }
       fs.writeFileSync(path.join(folderPath, 'source.js'), file.file)
       fs.writeFileSync(path.join(folderPath, 'config.json'), file.config)
       break;
